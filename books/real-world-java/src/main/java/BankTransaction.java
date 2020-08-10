eeaea434af4f1b539540be3ff787b85e0ba6fed8
Grace <govlmo91@gmail.com>
2020-08-11 08:17:54 +0900

    세번째 코드

---
+++ books/real-world-java/src/main/java/BankTransaction.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+
+/**
+ * @author : Eunmo Hong
+ * @since : 2020/08/10
+ */
+
+public class BankTransaction {
+
+	private final LocalDate date;
+	private final double amount;
+	private final String description;
+
+	public BankTransaction(LocalDate date, double amount, String description) {
+		this.date = date;
+		this.amount = amount;
+		this.description = description;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	@Override
+	public String toString() {
+		return "BankTransaction{" +
+				"date=" + date +
+				", amount=" + amount +
+				", description='" + description + '\'' +
+				'}';
+	}
+}
