b3ee4c717664f4e7fa61aa968f2d5d316791837f
Grace <govlmo91@gmail.com>
2020-08-11 17:50:24 +0900

    입출금 내역 분석기 확장판 첫번째 요구사항

--- books/real-world-java/src/main/java/extendbankstatementanalyzer/BankStatementProcessor.java
+++ books/real-world-java/src/main/java/extendbankstatementanalyzer/BankStatementProcessor.java
@@ -54,4 +54,24 @@ class BankStatementProcessor {
 		}
 		return result;
 	}
+
+	public List<BankTransaction> findTransactionsInMonth(final Month month) {
+		final List<BankTransaction> result = new ArrayList<>();
+		for (BankTransaction bankTransaction : bankTransactions) {
+			if (bankTransaction.getDate().getMonth() == month) {
+				result.add(bankTransaction);
+			}
+		}
+		return result;
+	}
+
+	public List<BankTransaction> findTransactionsInMonthAndGreater(final Month month, final int amount) {
+		final List<BankTransaction> result = new ArrayList<>();
+		for (BankTransaction bankTransaction : bankTransactions) {
+			if (bankTransaction.getDate().getMonth() == month && bankTransaction.getAmount() >= amount) {
+				result.add(bankTransaction);
+			}
+		}
+		return result;
+	}
 }
