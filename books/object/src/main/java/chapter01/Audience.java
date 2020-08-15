869e8662d0aa8edbf205411792c94036492b336e
Grace <govlmo91@gmail.com>
2020-08-16 07:21:34 +0900

    두번째 코드

--- books/object/src/main/java/chapter01/Audience.java
+++ books/object/src/main/java/chapter01/Audience.java
@@ -12,7 +12,16 @@ public class Audience {
 		this.bag = bag;
 	}
 
-	public Bag getBag() {
-		return this.bag;
+	public Long buy(Ticket ticket) {
+		if (bag.hasInvitation()) {
+			bag.setTicket(ticket);
+			return 0L;
+		}else {
+			bag.setTicket(ticket);
+			bag.minusAmount(ticket.getFee());
+			return ticket.getFee();
+		}
 	}
+
+
 }
